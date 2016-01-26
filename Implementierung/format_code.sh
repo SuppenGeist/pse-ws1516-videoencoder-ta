d190428c376ab8a1166659991fba2116e845bd6b
Johannes Werner <johs.werner@googlemail.com>
2016-01-26 21:40:05 +0100

    Add code format script

---
+++ Implementierung/format_code.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#This script formats the code in the src/ subdirectory according
+#to the google c++ coding style guide.
+#In order to be able to execute this script successfully you should have astyle
+#installed on your machine.
+#
+#Under Ubuntu you can get astyle with the following command:
+#
+#    sudo apt-get install astyle
+#
+#
+#Author:Johannes Werner
+
+AFLAGS="-t -v -r -xC100 -A14"
+
+#format all .cc files
+printf "\nFormatting all *.cc files...\n\n"
+astyle ${AFLAGS} "src/*.cpp" 
+
+
+#format all header files
+printf "\n\nFormatting all header files...\n\n"
+astyle ${AFLAGS} "src/*.h" 
+
+#delete all *.orig files created by astyle
+printf "\n\nDeleting all temporary files...\n"
+find src/ -name '*.orig' -delete
+printf "Done.\n"
