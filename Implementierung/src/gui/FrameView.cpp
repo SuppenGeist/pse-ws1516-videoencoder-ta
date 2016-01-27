b8f0a6b5f8e9be45462ce84002c1bf63e88613fc
SuppenGeist <johs.werner@googlemail.com>
2016-01-27 10:44:32 +0100

    Implement FrameView

--- Implementierung/src/gui/FrameView.cpp
+++ Implementierung/src/gui/FrameView.cpp
@@ -1,29 +1,89 @@
-/*#include <exception>
-
 #include "FrameView.h"
-#include "QWidget.h"
 
-FrameView::FrameView(QWidget* parent) {
+#include <QWidget>
+#include <QPainter>
+#include <QColor>
+#include <QPaintEvent>
+#include <QImage>
+#include <QRect>
+#include <QSize>
+
+GUI::FrameView::FrameView(QWidget* parent):QWidget(parent),xOffset_(0),yOffset_(0) {
+    setMinimumSize(100,100);
+    setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+    originalFrame_=new QImage(100,100,QImage::Format_RGB888);
+    for(int i=0;i<100;i++) {
+        for(int k=0;k<100;k++) {
+            originalFrame_->setPixel(i,k,QColor(64, 255, 64).rgb());
+        }
+    }
 }
 
-void FrameView::setFrame(QImage& frame) {
-	throw "Not yet implemented";
+void GUI::FrameView::setFrame(QImage& frame) {
+    originalFrame_=&frame;
+    currentFrame_=originalFrame_->scaled(width(),height(),Qt::KeepAspectRatio);
+
+    updateOffset();
+    update();
 }
 
-void FrameView::clear() {
-	throw "Not yet implemented";
+void GUI::FrameView::clear() {
+    originalFrame_=nullptr;
+    update();
 }
 
-void FrameView::resizeEvent(QResizeEvent* event) {
-	throw "Not yet implemented";
+void GUI::FrameView::resizeEvent(QResizeEvent* event) {
+    Q_UNUSED(event);
+
+    if( size().isEmpty() || !originalFrame_ )
+        return;
+
+    QSize eRect=event->size();
+    currentFrame_=originalFrame_->scaled(eRect.width(),eRect.height(),Qt::KeepAspectRatio);
+
+    updateOffset();
+    update();
 }
 
-void FrameView::repaintEvent(QPaintEvent* event) {
-	throw "Not yet implemented";
+void GUI::FrameView::paintEvent(QPaintEvent* event) {
+    if(visibleRegion().isEmpty())
+        return;
+
+    if(size().isEmpty())
+        return;
+
+   QPainter p(this);
+
+   //Draw the default background color
+   QColor defaultBackground(200,200,200);
+   p.fillRect(event->rect(),QBrush(defaultBackground));
+
+   //Draw the frame
+   if(originalFrame_) {
+       p.drawPixmap(xOffset_,yOffset_,currentFrame_.width(),currentFrame_.height(),QPixmap::fromImage(currentFrame_));
+   }
+
+   p.end();
 }
 
-void() FrameView::updateOffset() {
-	throw "Not yet implemented";
+void GUI::FrameView::updateOffset() {
+    if(!originalFrame_)
+        return;
+
+    if(width()>currentFrame_.width()) {
+        int diff=width()-currentFrame_.width();
+        xOffset_=diff/2;
+        yOffset_=0;
+    }
+    else if(height()>currentFrame_.height()){
+        int diff=height()-currentFrame_.height();
+        xOffset_=0;
+        yOffset_=diff/2;
+    }
+    else if(width()==currentFrame_.width() && height()==currentFrame_.height()) {
+        yOffset_=0;
+        xOffset_=0;
+    }
 }
 
-*/
+
--- Implementierung/src/gui/FrameView.h
+++ Implementierung/src/gui/FrameView.h
@@ -1,63 +1,61 @@
-/*
-#include <exception>
-using namespace std;
-
 #ifndef __FrameView_h__
 #define __FrameView_h__
 
-#include "QWidget.h"
+#include <QWidget>
 
 namespace GUI
 {
-	// class QWidget;
-	class FrameView;
-}
-
-namespace GUI
-{
-	/**
-	 * This class is the view used by the video player.
-	 * It automatically scales the frames passed to it.
-
-	class FrameView: public GUI::QWidget
+/**
+ * This class is the view used by the video player.
+ * It automatically scales the frames passed to it.
+*/
+    class FrameView: public QWidget
 	{
-		private: int xOffset;
-		private: int yOffset;
-		private: QImage currentFrame;
-		private: QImage* originalFrame;
+    public:
+        /**
+         * @brief FrameView Constructor.
+         * @param parent The parent widget.
+         */
+        FrameView(QWidget* parent = 0);
 
-		/// <summary>
-		/// Constructor.
-		/// </summary>
-		public: FrameView(QWidget* parent = 0);
+        /**
+         * @brief setFrame Sets the frame to show.
+         * @param frame The frame to show.
+         */
+        void setFrame(QImage& frame);
 
-		/// <summary>
-		/// Sets the frame to show.
-		/// </summary>
-		/// <param name="frame">The frame to show.</param>
-		public: void setFrame(QImage& frame);
+        /**
+         * @brief clear Clears the current frame so nothing is shown.
+         */
+        void clear();
 
-		/// <summary>
-		/// Clears the current frame so nothing is shown.
-		/// </summary>
-		public: void clear();
+    protected:
+        /**
+         * @brief resizeEvent This method is called when the widget got resized.
+         * @param event The resize event.
+         */
+        void resizeEvent(QResizeEvent* event);
 
-		/// <summary>
-		/// This method is called when the widget got resized.
-		/// </summary>
-		protected: void resizeEvent(QResizeEvent* event);
+        /**
+        * @brief repaintEvent This method is called when the widget has to be repainted.
+        * @param event The paint event.
+        */
+         void paintEvent(QPaintEvent* event);
 
-		/// <summary>
-		/// This method is called when the widget has to be repainted.
-		/// </summary>
-		protected: void repaintEvent(QPaintEvent* event);
 
-		/// <summary>
-		/// Updates the offset at which the image is drawn.
-		/// </summary>
-		private: void() updateOffset();
+    private:
+        int xOffset_;
+        int yOffset_;
+
+        QImage currentFrame_;
+        QImage* originalFrame_;
+
+        /**
+         * @brief updateOffset Updates the offset at which the image is drawn.
+         */
+        void updateOffset();
 	};
 }
 
 #endif
-*/
+
