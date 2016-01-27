d607e3ea61181e285cc288f1cfb3f6dc158727c4
SuppenGeist <johs.werner@googlemail.com>
2016-01-27 16:24:21 +0100

    Implement Player

--- Implementierung/src/gui/Player.h
+++ Implementierung/src/gui/Player.h
@@ -1,95 +1,87 @@
-/*
-#include <exception>
-using namespace std;
-
 #ifndef __Player_h__
 #define __Player_h__
-
-// #include "ControlPanel.h"
-
-namespace GUI
-{
-	class ControlPanel;
-	class Player;
-}
 
 namespace GUI
 {
 	/**
 	 * This class is the base class for players.
+     * It specifies the basic interface that a player has to provide.
 	 */
-	__abstract class Player
+    class Player
 	{
-		public: GUI::ControlPanel* players;
-		public: GUI::ControlPanel* masterPlayer;
+    public:
+        /**
+         * @brief ~Player Destructor.
+         */
+        virtual ~Player() {}
 
-		/// <summary>
-		/// Plays the video.
-		/// </summary>
-		public: virtual void play() = 0;
+        /**
+         * @brief play Plays the video.
+         */
+        virtual void play() = 0;
 
-		/// <summary>
-		/// Pauses the video.
-		/// </summary>
-		public: virtual void pause() = 0;
+        /**
+         * @brief pause Pauses the video.
+         */
+        virtual void pause() = 0;
 
-		/// <summary>
-		/// Stops the video.
-		/// </summary>
-		public: virtual void stop() = 0;
+        /**
+         * @brief stop Stops the video.
+         */
+        virtual void stop() = 0;
 
-		/// <summary>
-		/// Shows the next frame.
-		/// </summary>
-		public: virtual void nextFrame() = 0;
+        /**
+         * @brief nextFrame Shows the next frame.
+         */
+        virtual void nextFrame() = 0;
 
-		/// <summary>
-		/// Shows the previous frame.
-		/// </summary>
-		public: virtual void previousFrame() = 0;
+        /**
+         * @brief previousFrame Shows the previous frame.
+         */
+        virtual void previousFrame() = 0;
 
-		/// <summary>
-		/// Sets the speed.
-		/// </summary>
-		/// <param name="speed">The new speed.</param>
-		public: virtual void setSpeed(float speed) = 0;
+        /**
+         * @brief setSpeed Sets the speed.
+         * @param speed The new speed.
+         */
+        virtual void setSpeed(float speed) = 0;
 
-		/// <summary>
-		/// Sets the position in the video.
-		/// </summary>
-		/// <param name="position">The new position.</param>
-		public: virtual void setPosition(int position) = 0;
+        /**
+         * @brief setPosition Sets the position in the video.
+         * @param position The new position.
+         */
+        virtual void setPosition(int position) = 0;
 
-		/// <summary>
-		/// Returns the position in the video.
-		/// </summary>
-		/// <returns>The current position.</returns>
-		public: virtual int getPosition() = 0;
+        /**
+         * @brief getPosition Returns the position in the video.
+         * @return The current position.
+         */
+        virtual int getPosition() const noexcept = 0;
 
-		/// <summary>
-		/// Returns the speed.
-		/// </summary>
-		/// <returns>The current speed.</returns>
-		public: virtual float getSpeed() = 0;
+        /**
+         * @brief getSpeed Returns the speed.
+         * @return The current speed.
+         */
+        virtual float getSpeed() const noexcept = 0;
 
-		/// <summary>
-		/// Whether the player is currently playing.
-		/// </summary>
-		/// <returns>True if the player is playing.</returns>
-		public: virtual bool isPlaying() = 0;
+        /**
+         * @brief isPlaying Whether the player is currently playing.
+         * @return True if the player is playing.
+         */
+        virtual bool isPlaying() const noexcept = 0;
 
-		/// <summary>
-		/// Whether the player is stopped.
-		/// </summary>
-		/// <returns>True if the player is stopped.</returns>
-		public: virtual bool isStopped() = 0;
+        /**
+         * @brief isStopped Whether the player is stopped.
+         * @return True if the player is stopped.
+         */
+        virtual bool isStopped() const noexcept = 0;
 
-		/// <summary>
-		/// Resets the player.
-		/// </summary>
-		public: virtual void reset() = 0;
+        /**
+         * @brief reset Resets the player.
+         */
+        virtual void reset() = 0;
 	};
 }
 
 #endif
-*/
+
