1f6e06bc0241e956f59cc041bb74ffbeff4ec82c
Simon Grafenhorst <simon.grafenhorst@gmail.com>
2016-01-31 13:49:09 +0100

    Added av_codec general stuff You might have to fix the .pro file and all the following lines: unix: CONFIG += link_pkgconfig unix: PKGCONFIG += libavcodec
    unix: PKGCONFIG += libavformat
    
    unix: PKGCONFIG += libavutil
    unix: PKGCONFIG += libswscale
    
    LIBS += -ldl

--- Implementierung/src/main.cpp
+++ Implementierung/src/main.cpp
@@ -2,9 +2,13 @@
 #include "gui/MainWindow.h"
 #include "gui/FilterContainerTab.h"
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-
+    avcodec_register_all();
     GUI::MainWindow mw;
     mw.show();
 
