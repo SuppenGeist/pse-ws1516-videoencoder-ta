8353e570779faebd6c4426f5d073eeeaa23854aa
Simon Grafenhorst <simon.grafenhorst@gmail.com>
2016-02-03 17:00:46 +0100

    Implemented convertVideoToAVVideo

--- Implementierung/src/utility/VideoConverter.cpp
+++ Implementierung/src/utility/VideoConverter.cpp
@@ -1,11 +1,21 @@
-/*#include <exception>
-
 #include "VideoConverter.h"
-#include "Video.h"
-#include "AVVideo.h"
 
+static unique_ptr<QImage> convertAVFrameToQImage(AVFrame& frame, int width, int height){
 
+}
 
+static unique_ptr<Model::Video> convertAVVideoToVideo(Model::AVVideo& video){
 
+}
 
-*/
+static unique_ptr<AVFrame> convertQImageToAVFrame(QImage& image){
+
+}
+
+static unique_ptr<Model::AVVideo> convertVideoToAVVideo(Model::Video& video){
+    unique_ptr<Model::AVVideo> avvideo = make_unique<Model::AVVideo>(video.getFps(), video.getWidth(), video.getHeight());
+    for(int i = 0; i < video.getNumberOfFrames(); i++) {
+        avvideo->insertFrame(convertQImageToAVFrame(*video.getFrame(i)));
+    }
+    return avvideo;
+}
--- Implementierung/src/utility/VideoConverter.h
+++ Implementierung/src/utility/VideoConverter.h
@@ -1,30 +1,18 @@
-/*
 #include <exception>
 using namespace std;
 
 #ifndef __VideoConverter_h__
 #define __VideoConverter_h__
 
-#include "Video.h"
-#include "AVVideo.h"
+#include "../model/Video.h"
+#include "../model/AVVideo.h"
 
-namespace GUI
-{
-	class Video;
-}
-namespace Model
-{
-	class AVVideo;
-}
-namespace Utility
-{
-	class VideoConverter;
-}
 
 namespace Utility
 {
 	/**
 	 * Converts AVFrames to QImages and vice versa.
+     */
 
 	class VideoConverter
 	{
@@ -36,38 +24,30 @@ namespace Utility
 		/// <param name="width">The width of the frame.</param>
 		/// <param name="height">The height of the frame.</param>
 		/// <returns>The converted AVFrame.</returns>
-		public: static std::unique_ptr<QImage> convertAVFrameToQImage(AVFrame& frame, int width, int height) {
-			throw "Not yet implemented";
-		}
+        public: static unique_ptr<QImage> convertAVFrameToQImage(AVFrame& frame, int width, int height);
 
 		/// <summary>
 		/// Converts a AVVideo to a Video
 		/// </summary>
 		/// <param name="video">The video to convert.</param>
 		/// <returns>The converted AVVideo.</returns>
-		public: static std::unique_ptr<GUI::Video> convertAVVideoToVideo(Model::AVVideo& video) {
-			throw "Not yet implemented";
-		}
+        public: static unique_ptr<Model::Video> convertAVVideoToVideo(Model::AVVideo& video);
 
 		/// <summary>
 		/// Converts a qimage to a avframe.
 		/// </summary>
 		/// <param name="imgae">The qimage to convert.</param>
 		/// <returns>The converted qimage.</returns>
-		public: static unique_ptr<AVFrame> convertQImageToAVFrame(QImage& imgae) {
-			throw "Not yet implemented";
-		}
+        public: static unique_ptr<AVFrame> convertQImageToAVFrame(QImage& image);
 
 		/// <summary>
 		/// Converts a Video to a AVVideo.
 		/// </summary>
 		/// <param name="video">The video to convert.</param>
 		/// <returns>The converted video.</returns>
-		public: static unique_ptr<Model::AVVideo> convertVideoToAVVideo(GUI::Video& video) {
-			throw "Not yet implemented";
-		}
+        public: static unique_ptr<Model::AVVideo> convertVideoToAVVideo(Model::Video& video);
 	};
 }
 
 #endif
-*/
+
