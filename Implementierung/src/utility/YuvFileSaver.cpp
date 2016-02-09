c28628ddc5bd3d5b1bcaa91111737d983862d65a
Sascha Rapp <SaschaRapp95@gmx.de>
2016-02-09 19:08:01 +0100

    add conversion methods to yuvfilesaver

--- Implementierung/src/utility/YuvFileSaver.cpp
+++ Implementierung/src/utility/YuvFileSaver.cpp
@@ -13,3 +13,15 @@ Utility::YuvFileSaver::~YuvFileSaver() {
 	file_.flush();
 	file_.close();
 }
+
+int Utility::YuvFileSaver::RgbToY(QRgb pixel){
+   return 0.299*qRed(pixel)+ 0.587*qGreen(pixel) + 0.1144*qBlue(pixel);
+}
+
+int Utility::YuvFileSaver::RgbToU(QRgb pixel){
+   return (-0.147)*qRed(pixel)-0.289*qGreen(pixel)+0.436*qBlue(pixel);
+}
+
+int Utility::YuvFileSaver::RgbToV(QRgb pixel){
+    return 0.615*qRed(pixel)-0.515*qGreen(pixel)-0.100*qBlue(pixel);
+}
