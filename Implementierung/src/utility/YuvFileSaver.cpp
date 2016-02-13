a1cf6bd0b8c2de23f8ea277d339cae8fa36ae0fd
Sascha Rapp <SaschaRapp95@gmx.de>
2016-02-13 12:49:40 +0100

    change yuv to rgb formula

--- Implementierung/src/utility/YuvFileSaver.cpp
+++ Implementierung/src/utility/YuvFileSaver.cpp
@@ -15,13 +15,27 @@ Utility::YuvFileSaver::~YuvFileSaver() {
 }
 
 int Utility::YuvFileSaver::RgbToY(QRgb pixel) {
-	return 0.299*qRed(pixel)+ 0.587*qGreen(pixel) + 0.1144*qBlue(pixel);
+    return (0.257*qRed(pixel)) + (0.504*qGreen(pixel)) + (0.098*qBlue(pixel))+16;
 }
 
 int Utility::YuvFileSaver::RgbToU(QRgb pixel) {
-	return (-0.147)*qRed(pixel)-0.289*qGreen(pixel)+0.436*qBlue(pixel);
+    return ((-0.148)*qRed(pixel)) - (0.291*qGreen(pixel)) + (0.439*qBlue(pixel)) + 128;
 }
 
 int Utility::YuvFileSaver::RgbToV(QRgb pixel) {
-	return 0.615*qRed(pixel)-0.515*qGreen(pixel)-0.100*qBlue(pixel);
+    return (0.439*qRed(pixel)) - (0.368*qGreen(pixel)) - (0.071*qBlue(pixel)) + 128;
 }
+
+/*
+int Utility::YuvFileSaver::RgbToY(QRgb pixel) {
+    return 0.299*qRed(pixel)+ 0.587*qGreen(pixel) + 0.1144*qBlue(pixel);
+}
+
+int Utility::YuvFileSaver::RgbToU(QRgb pixel) {
+    return (-0.147)*qRed(pixel)-0.289*qGreen(pixel)+0.436*qBlue(pixel);
+}
+
+int Utility::YuvFileSaver::RgbToV(QRgb pixel) {
+    return 0.615*qRed(pixel)-0.515*qGreen(pixel)-0.100*qBlue(pixel);
+}
+*/
