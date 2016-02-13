249cb44f9306afce4f4ae688a07b2738ef75ca56
Simon Grafenhorst <simon.grafenhorst@gmail.com>
2016-02-13 15:26:20 +0100

    Corrected loading and decoding

--- Implementierung/src/utility/VideoLoader.cpp
+++ Implementierung/src/utility/VideoLoader.cpp
@@ -31,6 +31,7 @@ unique_ptr<Model::AVVideo> VideoLoader::loadVideo() {
 	// Print stream information
 	av_dump_format(formatContext, 0, path.toUtf8(), 0);
 
+
 	// Find the best video stream in context
 	int videoStreamIndex = av_find_best_stream(formatContext, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
 	if(videoStreamIndex == -1) {
@@ -39,6 +40,7 @@ unique_ptr<Model::AVVideo> VideoLoader::loadVideo() {
 
 	// Get a pointer to the codec context for the video stream
 	codecContext = formatContext->streams[videoStreamIndex]->codec;
+
 
 	// Find the decoder for the video stream
 	codec = avcodec_find_decoder(codecContext->codec_id);
@@ -50,15 +52,21 @@ unique_ptr<Model::AVVideo> VideoLoader::loadVideo() {
 	if(avcodec_open2(codecContext, codec, NULL) < 0)
 		return NULL; // Could not open codec
 
-	AVPacket *packet = NULL;
-	AVFrame *frame = NULL;
+    AVPacket packet;
+    AVFrame *frame = NULL;
 	unique_ptr<Model::AVVideo> video = make_unique<Model::AVVideo>(codecContext->framerate.num);
-
-	while(av_read_frame(formatContext, packet) >= 0) {
-		frame = av_frame_alloc();
-		avcodec_decode_video2(codecContext, frame, 0, packet);
-		video->appendFrame(frame);
+    av_init_packet(&packet);
+    packet.data = NULL;
+    packet.size = 0;
+    int gotPicture = 0;
+    while(av_read_frame(formatContext, &packet) >= 0) {
+        frame = new AVFrame();
+        frame = av_frame_alloc();
+        avcodec_decode_video2(codecContext, frame, &gotPicture, &packet);
+        if(gotPicture != 0) {
+            video->appendFrame(frame);
+        }
 	}
 
-	return video;
+    return std::move(video);
 }
