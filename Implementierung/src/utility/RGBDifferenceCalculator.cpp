9d51c7cb15f68f2315751ee6c82427a0f434baea
Carina <carifle33@googlemail.com>
2016-02-17 01:23:40 +0100

    Implement calculate Video

--- Implementierung/src/utility/RGBDifferenceCalculator.cpp
+++ Implementierung/src/utility/RGBDifferenceCalculator.cpp
@@ -1,5 +1,9 @@
 #include "RGBDifferenceCalculator.h"
 
+#include "../model/Video.h"
+
+#include <QColor>
+
 Utility::RGBDifferenceCalculator::RGBDifferenceCalculator(Model::Video& referenceVideo,
         Model::Video& video) {
 	this->referenceVideo = &referenceVideo;
@@ -7,5 +11,27 @@ Utility::RGBDifferenceCalculator::RGBDifferenceCalculator(Model::Video& referenc
 }
 
 void Utility::RGBDifferenceCalculator::calculateVideo(Model::Video& target) {
-	//TODO: Calculate
+    isRunning = true;
+    std::size_t i=0;
+    do {
+        for(; i < video_->getNumberOfFrames()&&isRunning_; i++) {
+            QImage *currentFrame = video_->getFrame(i);
+            QImage *refCurrentFrame = referenceVideo_->getFrame(i);
+            auto *outFrame = new QImage(currentFrame->getHeight(), currentFrame->getWidth(), currentFrame->format);
+            for(int j = 0; j < currentFrame->height(); j++) {
+                for(int k = 0; k < currentFrame->width(); k++) {
+                    auto pixel=currentFrame->pixel(k,j);
+                    auto refPixel=refCurrentFrame->pixel(k,j);
+                    int blue =  std::abs (qBlue(pixel)-qBlue(refPixel));
+                    int red =  std::abs (qRed(pixel)-qRed(refPixel));
+                    int green =  std::abs (qGreen(pixel)-qGreen(refPixel));
+                    outFrame->setPixel(j,k, qRGB(red, green, blue));
+
+                }
+            }
+            target->insertFrame(*outFrame,i);
+
+        }
+    } while (isRunning_&&!video_->isComplete()&&!referenceVideo->isComplete());)
+    isRunning = false;
 }
